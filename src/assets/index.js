94a709b336c0c589f7f5dc247b7378e650c8ffff
Marleen ( 玛琳) <handyyou@live.nl>
2023-08-02 15:28:31 +0200

    Website is done, needs more personal info, will add later

--- src/assets/index.js
+++ src/assets/index.js
@@ -21,10 +21,8 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
+import bronnen from "./company/bronnen.png";
+import eduhint from "./company/eduhint.png";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
@@ -52,10 +50,8 @@ export {
   tailwind,
   typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
+  bronnen,
+  eduhint,
   carrent,
   jobit,
   tripguide,
