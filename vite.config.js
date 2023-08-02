a1da596d46270c20a391346bb473db922f9cee66
Marleen ( 玛琳) <handyyou@live.nl>
2023-08-03 00:44:29 +0200

    github pages finally working

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/portfolio/',
   plugins: [react()],
   server: {
+    // Add the custom MIME type for JSX files
     mimeTypes: {
-      // Change the MIME type for JavaScript files
-      'js': 'application/javascript',
+      'jsx': 'module/javascript',
     },
   },
 });
