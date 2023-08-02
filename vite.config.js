4c015a4a9411eeced59dfbe0649feccf3893d4d1
Marleen ( 玛琳) <handyyou@live.nl>
2023-08-03 00:23:08 +0200

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    mimeTypes: {
+      // Change the MIME type for JavaScript files
+      'js': 'application/javascript',
+    },
+  },
+});
